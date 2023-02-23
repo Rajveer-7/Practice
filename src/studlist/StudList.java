2ad43ace2407d5843eb015d5161122ce5cc1ea3a
Rajveer-7 <sin12051@sheridancollege.ca>
2023-02-23 12:05:23 -0500

    commit 3

--- src/studlist/StudList.java
+++ src/studlist/StudList.java
@@ -13,6 +13,17 @@ public class StudList {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+        
          private String Prince;
 
     public String getPrince() {
